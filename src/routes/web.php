6935025233b4b5744137d93dcf630dd146e82fbf
nicola.eliseo@gbrain.it <nicola.eliseo@gbrain.it>
2024-04-02 09:34:42 +0200

    esercizio 2 salutaNome

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\API\SalutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/info', function () {
     return 'Benvenuto nell\'applicazione Laravel!';
 });
+
+Route::get('/saluta/{nome}', [SalutoController::class,'salutaNome']);
