34328ec1bd5298592471a37637f2a095b6a6771a
nicola.eliseo@gbrain.it <nicola.eliseo@gbrain.it>
2024-04-05 09:51:48 +0200

    esercizio 3.1 migration,model,controller,request films

--- src/app/Http/Requests/StoreFilmRequest.php
+++ src/app/Http/Requests/StoreFilmRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreFilmRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class StoreFilmRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,16 @@ class StoreFilmRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'titolo' => 'required|max:150',
+            'trama' => 'nullable',
+            'anno' => 'required',
         ];
+    }
+
+    public function failedValidation(Validator $validator){
+        throw new HttpResponseException(response()->json([
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ], 400));
     }
 }
