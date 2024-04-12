5b191ff1efad0fd218a7991e0b6e6141cd2d0c59
nicola.eliseo@gbrain.it <nicola.eliseo@gbrain.it>
2024-04-12 10:02:20 +0200

    crud regista completed

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\FilmController;
+use App\Http\Controllers\RegistaController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('films', FilmController::class)->only([
+    'index', 'store', 'show', 'update', 'destroy'
+]);
+
+Route::resource('regista', RegistaController::class)->only([
     'index', 'store', 'show', 'update', 'destroy'
 ]);
